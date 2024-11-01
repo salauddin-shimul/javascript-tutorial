3e02e63f8004e8ed16253486edb66587729125dc
SM Salauddin Shimul <sm.salauddin.shimul@g.bracu.ac.bd>
2024-11-01 12:51:21 +0600

    learned window alert and commenting in js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!")
+console.log("Hello, World!");
+
+window.alert(`This is an alert!`);
+window.alert(`I like pasta!!!`);
+
+// This is a comment
+
+/* 
+    This is
+    a multi-line
+    comment!
+*/
